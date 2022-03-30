a41a3126323520dce134778a3844ede414870477
hulinfeng123 <47943254+hulinfeng123@users.noreply.github.com>
2022-03-30 19:28:33 +0800

    Update BFS.java

--- src/BinaryTree/BFS.java
+++ src/BinaryTree/BFS.java
@@ -19,6 +19,8 @@ public class BFS {
         System.out.println("master");
         System.out.println("hot-fix");
         System.out.println("push test2");
+        System.out.println("pull test1");
+
 
 
         return result;
